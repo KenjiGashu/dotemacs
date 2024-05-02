6f47ce951d9b4c4b6615e3ab630cf03a68a34bcb
KenjiGashu <kenjigashu@gmail.com>
2024-05-01 21:06:33 -0300

    some more updates

--- .gashu/undo.el
+++ .gashu/undo.el
@@ -2,8 +2,9 @@
   :after evil
   :config
   (global-undo-tree-mode 1)
-  (evil-set-undo-system 'undo-tree))
+  (evil-set-undo-system 'undo-tree)
+	(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo"))))
 
-(use-package vundo
-  :config
-  (setq vundo-glyph-alist vundo-unicode-symbols))
+;; (use-package vundo
+;;   :config
+;;   (setq vundo-glyph-alist vundo-unicode-symbols))
--- .gashu/which-key.el
+++ .gashu/which-key.el
@@ -3,9 +3,10 @@
   (which-key-mode)
   :config
   (which-key-setup-side-window-right-bottom)
-  (setq which-key-sort-order 'which-key-key-order-alpha
-        which-key-side-window-max-width 0.33
-        which-key-idle-delay 0.05)
-  )
+	:custom
+	(which-key-sort-order 'which-key-key-order-alpha)
+	(which-key-side-window-max-width 1.00)
+  (which-key-idle-delay 0.05)
+	)
 ;; make which key show up faster
-(setq which-key-idle-delay 0.3) ;; I need the help, I really do
+;;(setq which-key-idle-delay 0.1) ;; I need the help, I really do
