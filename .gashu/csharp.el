8ff39dc60a68d5186b448d6f29f2ad27d6d20e37
lucas kenji gashu <kenjigashu@gmail.com>
2026-06-05 18:47:14 -0300

    properly config elpaca and csharp

--- .gashu/csharp.el
+++ .gashu/csharp.el
@@ -1,6 +1,10 @@
 ;;(add-hook 'csharp-mode-hook 'eglot-ensure)
-(add-hook 'csharp-mode-hook 'lsp)
-(add-hook 'csharp-mode-hook 'corfu-mode)
+(use-package emacs
+	:after (lsp eglot)
+	:init
+	(add-hook 'csharp-mode-hook 'lsp)
+	(add-hook 'csharp-mode-hook 'corfu-mode)
 
-(add-to-list 'lsp-disabled-clients 'csharp-ls)
-(add-to-list 'lsp-disabled-clients 'csharp-roslyn)
+	(add-to-list 'lsp-disabled-clients 'csharp-ls)
+	(add-to-list 'lsp-disabled-clients 'csharp-roslyn)
+)
